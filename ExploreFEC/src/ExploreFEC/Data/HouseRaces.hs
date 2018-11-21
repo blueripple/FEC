dd1de57a8de16d7cd307943b7a79f064044a03ba
adamConnerSax <adam_conner_sax@yahoo.com>
2018-11-20 20:02:05 -0500

    Put (empty) HouseRaces.hs back in

---
+++ ExploreFEC/src/ExploreFEC/Data/HouseRaces.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE KindSignatures      #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+module ExploreFEC.Data.HouseRaces where
+
+import           Servant.API
+import           Servant.Client
+
+import           Data.Text
